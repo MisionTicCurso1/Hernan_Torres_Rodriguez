b5e774d9d0910d19329f62b4c53678b33e706166
Nanchixx <hetoro1703@gmail.com>
2021-08-30 18:16:12 -0500

    mensaje

--- Codigo/reto2.py
+++ Codigo/reto2.py
@@ -1,14 +1,7 @@
 def parametros (idEstudiante,notaUno,notaDos,notaTres,notaCuatro,canNotas):
     promedio = (notaUno + notaDos + notaTres + notaCuatro)/canNotas
-    estudiante = {
-    'codigo estudiante' : idEstudiante,
-    'nota uno' : notas[0],
-    'nota dos' : notas[1],
-    'nota tres' : notas[2],
-    'nota cuatro' : notas[3],
-    'promedio' : promedio
-    }
-    print(estudiante);
+    mensajeFinal = "El promedio ajustado del estudiante ", idEstudiante, " es: ", promedio
+    return mensajeFinal
 codigo = input("Ingrese el codigo del estudiante a conocer su promedio ")
 notas = []
 for i in range(0,5):
@@ -18,4 +11,5 @@ for i in range(0,5):
 notas.sort()
 notas.pop(0)
 canNotas = len(notas)
-parametros(codigo,notas[0],notas[1],notas[2],notas[3],canNotas)
+mensaje = parametros(codigo,notas[0],notas[1],notas[2],notas[3],canNotas)
+print(mensaje)
